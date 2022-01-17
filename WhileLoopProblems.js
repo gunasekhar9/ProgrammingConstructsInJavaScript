f8331cde414ff9b867c94bd85f5233ea44553f42
gunasekhar9 <gunasekhar1539@gmail.com>
2022-01-17 18:59:37 +0530

    [gunasekhar9] add : Problems Using While Loop is Done

---
+++ WhileLoopProblems.js
@@ -0,0 +1,16 @@
+// Flip coin
+console.log("--- For Flip Coin ---");
+let headCount=0;
+let tailCount=0;
+while(headCount >= 11 || tailCount >= 11)
+{
+    let CoinToss = Math.random();
+    if(CoinToss >= 0.5)
+       headCount++;
+    else
+       tailCount++;
+}
+if(headCount == 11)
+    console.log("Head Wins");
+else
+    console.log("Tail Wins");
